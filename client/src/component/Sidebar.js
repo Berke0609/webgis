7616d6dcff0b0c46912c97dfc15d0e3253c28d51
Berke Aygoren <92017501+Berke0609@users.noreply.github.com>
2023-05-30 22:08:21 +0300

    Update Sidebar.js

--- client/src/component/Sidebar.js
+++ client/src/component/Sidebar.js
@@ -24,7 +24,7 @@ const Sidebar = ({ category }) => {
         const { latitude, longitude } = position.coords;
   
         try {
-          const response = await fetch('http://localhost:10000/item', {
+          const response = await fetch('https://nodebeckend-production.up.railway.app/item', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
